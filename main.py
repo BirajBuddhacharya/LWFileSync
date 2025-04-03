4d0ad8ec451ac3309a5c1210ee55fb72b3b5215b
Biraj Buddhacharya <otaku000god@gmail.com>
2025-04-04 00:21:35 +0545

    Minor tweaks

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 from rich.logging import RichHandler
 import config
 from concurrent.futures import ThreadPoolExecutor
-from utils import mount_device, src_dicts, dist_dicts, sync_loop, test
+from utils import mount_device, sync_loop, preprocess_path
 
 logging.basicConfig(level=logging.DEBUG, handlers=[RichHandler()])
 
@@ -10,12 +10,13 @@ def main():
    # mounting device 
    mount_device()
    
+   # preprocessing dict path
+   preprocess_path()
+   
    # syncing file 
    with ThreadPoolExecutor(max_workers=10) as executor: 
-       executor.map(sync_loop, src_dicts, dist_dicts)
+       executor.map(sync_loop, config.src_dicts, config.dist_dicts)
 
 if __name__ == '__main__': 
-    logging.info(config.device)
-    test()
-    logging.info(config.device)
+    pass
     
